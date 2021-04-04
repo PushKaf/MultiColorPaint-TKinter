3aa22bb8ec8fee109d59c2f2edd53a25e83c785e
PyroGamez <pushkal.kafley@gmail.com>
2021-04-04 17:34:42 -0400

    better with color picker

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from tkinter import *
-
+from tkinter import colorchooser
 
 #current x and y cords of the mouse
 curX, curY = 0, 0
-#all the colors to init
-colors = ["black", "gray", "darkslategray", "slategray", "gray", "lightgrey", "blue", "royalblue", "dodgerblue", "deepskyblue1", "cyan2", "seagreen1", "green", "green2", "limegreen", "red", "orangered", "indianred1", "yellow", "gold", "orange"]
+#current color when first init
 currColor = "black"
 
 #get where the mouse currently is
@@ -18,24 +17,13 @@ def addLine(event):
     canvas.create_line((curX, curY, event.x, event.y), fill=currColor)
     curX, curY = event.x, event.y
 
-#make rectangles cordinating to the colors, and bind tags to the colors when clicked
-def createRect():
-    x, y, x1, y1 = 10, 10, 35, 35
-    for color in colors:
-        colorRect = canvas.create_rectangle((x, y, x1, y1), fill=color)
-        bindTag(colorRect ,color)
-        y+=30  
-        y1+=30
-
-#NOTE I tried to do this in the for loop ^ , but didnt really work...
-def bindTag(rect, color):
-    #bind the tag when clicked change the color
-    canvas.tag_bind(rect, "<Button-1>", lambda x: changeColor(color))
-
-#set the current color of the line to the coordinated color of the rectangle clicked on 
-def changeColor(newColor):
+#open the color picker menu 
+def colorPicker():
     global currColor
-    currColor = newColor
+
+    color_code = colorchooser.askcolor(title ="Choose color")    
+    currColor = color_code[1]
+
 
 #init the main window
 root = Tk()
@@ -48,11 +36,15 @@ root.columnconfigure(0, weight=1)
 canvas = Canvas(root)
 canvas.grid(row=0, column=0, sticky="nsew")
 
+#add the button to pick colors
+colorPickBTN = Button(canvas, text="Pick Color", command=colorPicker)
+colorPickBTN.grid(row=0, column=0)
+
 #bind the buttons and drags
 canvas.bind("<Button-1>", locXY)
 canvas.bind("<B1-Motion>", addLine)
 
-createRect()
+# createRect()
 
 
 root.mainloop()
